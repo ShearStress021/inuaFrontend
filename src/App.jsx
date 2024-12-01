61e5518a7fe5d3b75638fed28e136b8145e6c73e
ShearStress021 <shearstress021@gmail.com>
2024-12-01 12:04:09 -0800

    frontenv

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [array, setArray] = useState([])
+
+
+  const API = import.meta.env.VITE_API_URL
+
+  const fetchApi = async () => {
+    const response = await axios.get(`${API}/users`)
+
+    setArray(response.data.users)
+  }
+
+  useEffect(() => {
+    fetchApi();
+  }, [API])
+  return (
+    <>
+      <ul>
+        {
+          array.map((user, index) => (
+            <li key={index}>{user}</li>
+          ))
+        }
+      </ul>
+    </>
+  )
+}
+
+export default App
